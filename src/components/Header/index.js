9e705372ff856890df16163e714218a83e04a489
n1h41 <nihalninu25@gmail.com>
2021-08-16 20:58:18 +0530

    laptop png added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,75 +1,85 @@
-import {React, useState} from 'react'
+import { React, useState } from 'react'
 import { Container, Row, Col, Nav, Navbar } from 'react-bootstrap'
 import Logo from '../../assets/logo.png'
 import './index.css'
 
 function Header() {
     const [cartExpand, setCartExpand] = useState(false)
+    const [scrolled, setScrolled] = useState(false)
+    const scrollChanged = () => {
+        if (window.scrollY > 125)
+            setScrolled(true)
+        else
+            setScrolled(false)
+    }
+    window.addEventListener('scroll', scrollChanged)
     return (
         <>
-            <Container className="top-bar py-3">
-                <Row className="d-flex align-items-center">
-                    <Col>
-                        Welcome to Altelal Online Shopping Store !
-                    </Col>
-                    <Col className="d-flex justify-content-between align-items-center">
-                        <select name="language" id="language" className="lang-selector">
-                            <option value="en|en">English</option>
-                            <option value="en|ar">Arabic</option>
-                        </select>
-                        <div className="v-d"></div>
-                        <select name="currency" id="currency" className="currency-selector">
-                            <option value="USD">United States (US) Dollar ($)</option>
-                            <option value="DHR">United Arab Emirates Dhirham</option>
-                        </select>
-                        <div className="v-d"></div>
-                        <Nav.Link href="#Home" className="p-0">Track Your Order</Nav.Link>
-                    </Col>
-                </Row>
+            <Container fluid className={`p-0 d-flex flex-column align-items-center sticky`}>
+                <Container className={`py-3 ${scrolled ? 'top-bar d-none' : 'top-bar'}`}>
+                    <Row className="d-flex align-items-center">
+                        <Col>
+                            Welcome to Altelal Online Shopping Store !
+                        </Col>
+                        <Col className="d-flex justify-content-between align-items-center">
+                            <select name="language" id="language" className="lang-selector">
+                                <option value="en|en">English</option>
+                                <option value="en|ar">Arabic</option>
+                            </select>
+                            <div className="v-d"></div>
+                            <select name="currency" id="currency" className="currency-selector">
+                                <option value="USD">United States (US) Dollar ($)</option>
+                                <option value="DHR">United Arab Emirates Dhirham</option>
+                            </select>
+                            <div className="v-d"></div>
+                            <Nav.Link href="#Home" className="p-0">Track Your Order</Nav.Link>
+                        </Col>
+                    </Row>
+                </Container>
+                <hr className="h-d" />
+                <Container className={`w-100 m-0 p-0 navb`}>
+                    <Navbar className="p-0 w-100 justify-content-between align-items-center" /* style={{justifyContent:"space-around"}} */>
+                        <div className="logo-container">
+                            <Navbar.Brand className="logo">
+                                <img src={Logo} alt="logo" className="logo" />
+                            </Navbar.Brand>
+                        </div>
+                        <ul className="extras-menu-container d-flex justify-content-around align-items-center m-0">
+                            <li className="counter-container">
+                                <a href="#home">
+                                    <span className="material-icons-outlined">
+                                        favorite_border
+                                    </span>
+                                </a>
+                                <span className="item-counter text-center">0</span>
+                            </li>
+                            <div className="v-d"></div>
+                            <li className="counter-container">
+                                <a href="#home" onMouseOver={() => setCartExpand(true)} onMouseLeave={() => setCartExpand(false)}>
+                                    <span className="material-icons-outlined">
+                                        shopping_cart
+                                    </span>
+                                </a>
+                                <span className="item-counter text-center">0</span>
+                                <div className={`d-flex justify-content-start p-2 ${cartExpand ? 'cart-preview' : 'cart-preview d-none'}`}>
+                                    <p>
+                                        No products in cart.
+                                    </p>
+                                </div>
+                            </li>
+                            <div className="v-d"></div>
+                            <li>
+                                <a href="#home">
+                                    <span className="material-icons-outlined">
+                                        person
+                                    </span>
+                                </a>
+                            </li>
+                        </ul>
+                    </Navbar>
+                </Container>
+                <hr className="h-d" />
             </Container>
-            <hr className="h-d" />
-            <Container>
-                <Navbar className="p-0 w-100 justify-content-between align-items-center">
-                    <div className="logo-container">
-                        <Navbar.Brand className="logo">
-                            <img src={Logo} alt="logo" className="logo" />
-                        </Navbar.Brand>
-                    </div>
-                    <ul className="extras-menu-container d-flex justify-content-around m-0">
-                        <li className="counter-container">
-                            <a href="#home">
-                                <span className="material-icons">
-                                    favorite_border
-                                </span>
-                            </a>
-                            <span className="item-counter text-center">0</span>
-                        </li>
-                        <div className="v-d"></div>
-                        <li className="counter-container">
-                            <a href="#home" onMouseOver={ () => setCartExpand(true)} onMouseLeave={ () => setCartExpand(false)}>
-                                <span className="material-icons-outlined">
-                                    shopping_cart
-                                </span>
-                            </a>
-                            <span className="item-counter text-center">0</span>
-                            <div className={`d-flex justify-content-start p-2 ${cartExpand ? 'cart-preview' : 'cart-preview d-none'}`}>
-                                <p>
-                                    No products in cart.
-                                </p>
-                            </div>
-                        </li>
-                        <div className="v-d"></div>
-                        <li>
-                            <a href="#home">
-                                <span className="material-icons-outlined">
-                                    person
-                                </span>
-                            </a>
-                        </li>
-                    </ul>
-                </Navbar>
-            </Container>
-            <hr className="h-d" />
         </>
     )
 }
--- src/components/HeroContent/index.js
+++ src/components/HeroContent/index.js
@@ -1,58 +1,83 @@
 import { React, useState } from 'react'
 import './index.css'
-import { Row, Col, Container, Form } from 'react-bootstrap'
+import { Row, Col, Container, Form, Button } from 'react-bootstrap'
 import ReactPlayer from 'react-player'
-import Magnifier from "react-magnifier";
+import 'date-fns';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+    MuiPickersUtilsProvider,
+    KeyboardTimePicker,
+    KeyboardDatePicker,
+} from '@material-ui/pickers';
+import laptop from '../../assets/laptop.png'
 
 function HeroContent() {
-    const kanduraImgData = [
-        'https://cdn.shopify.com/s/files/1/0274/4841/0212/products/telalthobe_1_2d8c0ea4-cb47-4bd8-b171-e45dffb86d97_grande.jpg?v=1593930137',
-        'https://cdn.shopify.com/s/files/1/0274/4841/0212/products/telalthobe_4_12a90a3b-e5b0-4210-b588-080ddd1d94d8_grande.jpg?v=1593930137',
-        'https://cdn.shopify.com/s/files/1/0274/4841/0212/products/telalthobe_3_04c4f6d8-4b41-4793-a988-35649e18059a_grande.jpg?v=1593930137',
-        'https://cdn.shopify.com/s/files/1/0274/4841/0212/products/telalthobe_2_b190c09e-ce81-4a43-af88-cda7cf3c7ffc_grande.jpg?v=1593930137',
-        'https://cdn.shopify.com/s/files/1/0274/4841/0212/products/telaldishdash_1_80c11f9f-cbdb-4546-ae22-efb508138dc2_grande.jpg?v=1593930137',
-        'https://cdn.shopify.com/s/files/1/0274/4841/0212/products/telaldishdash_4_249a9920-4244-4b3d-9f02-e42914e7c5ea_grande.jpg?v=1593930137',
-        'https://cdn.shopify.com/s/files/1/0274/4841/0212/products/telaldishdash_2_a1ce1bc7-6562-4642-b0c9-498f0506b919_grande.jpg?v=1593930137',
-        'https://cdn.shopify.com/s/files/1/0274/4841/0212/products/telaldishdash_3_690e5c02-1dd5-454b-99ba-3cf74f7bc2fc_grande.jpg?v=1593930137',
-    ]
-    const [selectedSrc, setselectedSrc] = useState(kanduraImgData[0])
+    const [selectedDate, setSelectedDate] = useState(Date.now());
+
+    const handleDateChange = (date) => {
+        console.log(date)
+        setSelectedDate(date);
+    };
     return (
         <>
             <hr className="h-d" />
-            <Container className="hero-container pt-5">
-                <Container>
-                    <ReactPlayer width='100%' controls={true} pip={true} /* playing={true} */ url='https://www.youtube.com/watch?v=dQw4w9WgXcQ' />
-                </Container>
-                <Container className="ps-4 pt-5 pb-2">
+            <Container className="hero-container pt-4">
+                <Container className="ps-4 pb-2">
                     <p style={{ color: "#403b37" }}>Home {'>'} KanduraClassic-Adult</p>
                 </Container>
                 <Row>
-                    <Col lg="6" xs="12" className="image-section">
-                        <Container>
-                        <Magnifier className="w-100" src={selectedSrc} width={500} />
-                            {/* <img src={selectedSrc} alt="main-pic" className="w-100 main-image-container" /> */}
-                        </Container>
-                        <Container className="pt-2 w-100x">
-                            <Row className="pt-2">
-                                {kanduraImgData.map((src) =>
-                                    <Col xs="4" lg="3" className="mb-3" key={src}>
-                                        <img key={src} className="w-100" src={src} alt="mini-pics" onClick={() => { setselectedSrc(src) }} />
-                                    </Col>
-                                )}
-                            </Row>
+                    <Col lg="6" xs="12" className="video-section justify-content-center d-flex flex-column">
+                        <Container className="video-container">
+                            <img src={laptop} alt="laptop"  className="w-100 laptop-conatiner"/>
+                            <ReactPlayer /* width="68%" height="77%" */  controls={true} pip={true} url='https://www.youtube.com/watch?v=dQw4w9WgXcQ' className="video-player"/>
                         </Container>
                     </Col>
                     <Col lg="6" xs="12" className="product-description">
                         <Container className="pt-4">
-                            <span className="">Telal</span>
-                            <h3 className="product-title">Kandura Classic - Adult</h3>
-                            <Container className="ps-0">
-                                <span className="product-price">Dhs 189.00</span>
-                                <div className="product-policy">Tax included</div>
-                            </Container>
                             <Form>
-                                <Form.Group>
+                                <Row className="mb-3">
+                                    <Form.Group as={Col} controlId="formGridFirstName">
+                                        <Form.Label>First Name</Form.Label>
+                                        <Form.Control type="text" placeholder="First Name" />
+                                    </Form.Group>
+                                    <Form.Group as={Col} controlId="formGridLastName">
+                                        <Form.Label>Last Name</Form.Label>
+                                        <Form.Control type="text" placeholder="Last Name" />
+                                    </Form.Group>
+                                </Row>
+                                <Form.Group className="mb-3" controlId="formGridEmail">
+                                    <Form.Label>Email</Form.Label>
+                                    <Form.Control type="email" placeholder="Email" />
                                 </Form.Group>
+                                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                    <Grid container justifyContent="space-between" className="pb-3">
+                                        <KeyboardDatePicker
+                                            margin="normal"
+                                            id="date-picker-dialog"
+                                            label="Date picker dialog"
+                                            format="MM/dd/yyyy"
+                                            value={selectedDate}
+                                            onChange={handleDateChange}
+                                            KeyboardButtonProps={{
+                                                'aria-label': 'change date',
+                                            }}
+                                        />
+                                        <KeyboardTimePicker
+                                            margin="normal"
+                                            id="time-picker"
+                                            label="Time picker"
+                                            value={selectedDate}
+                                            onChange={handleDateChange}
+                                            KeyboardButtonProps={{
+                                                'aria-label': 'change time',
+                                            }}
+                                        />
+                                    </Grid>
+                                </MuiPickersUtilsProvider>
+                                <Button variant="outline-primary" type="submit" className="submit-btn">
+                                    Book Now
+                                </Button>
                             </Form>
                         </Container>
                     </Col>
